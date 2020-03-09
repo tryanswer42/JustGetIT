ca8a816ccb250291dd6f2771c7f058ac1a98289d
luka.cukeric <lukacukeric77@gmail.com>
2020-03-09 16:03:09 +0100

    Made: - CategorieClass - Repository interface - JDBC implementation - Checked with dtbs - fields should be ok
    Started
    - Pagina Categorien (sub) maken en winstmarge

--- src/main/java/be/vdab/justgetit/controllers/CategorieenToevoegenController.java
+++ src/main/java/be/vdab/justgetit/controllers/CategorieenToevoegenController.java
@@ -1,9 +1,10 @@
 package be.vdab.justgetit.controllers;
 
 import be.vdab.justgetit.domain.Categorie;
+import be.vdab.justgetit.repositories.CategorieRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,11 +14,22 @@ import java.math.BigDecimal;
 @RequestMapping("categorieen")
 public class CategorieenToevoegenController {
 
-@GetMapping
+    private final CategorieRepository categorieRepository;
+
+    public CategorieenToevoegenController(CategorieRepository categorieRepository) {
+        this.categorieRepository = categorieRepository;
+    }
+
+    @GetMapping
     public ModelAndView showForm(){
+
     ModelAndView modelAndView = new ModelAndView("categorieen");
-    modelAndView.addObject(new Categorie(0, "", 0, false, false, BigDecimal.ZERO ));
+    modelAndView.addObject("categorieList", categorieRepository.findAll());
+    modelAndView.addObject("categor",new Categorie(0, "", 0, false, false, BigDecimal.ZERO ));
     return modelAndView;
 }
+
+@PostMapping
+    public  ModelAndView categorieToevoegen();
 
 }
