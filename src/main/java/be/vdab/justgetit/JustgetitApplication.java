06bdc8e1dca68646108897a4a00dfe1d7755e57b
Mulangu <tryanswer42@gmail.com>
2020-03-09 09:29:12 +0100

    Start Project

---
+++ src/main/java/be/vdab/justgetit/JustgetitApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.justgetit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustgetitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustgetitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/justgetit/JustgetitApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.justgetit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustgetitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
