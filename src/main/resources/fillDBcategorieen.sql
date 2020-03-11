32b500cec9b874a5ff4182998d1275161f100e2d
andre.komdeur <andre.komdeur@gmail.com>
2020-03-11 14:33:37 +0100

    upvullen DB tabel categorieen.

---
+++ src/main/resources/fillDBcategorieen.sql
@@ -0,0 +1,21 @@
+LOCK TABLES `categorieen` WRITE;
+/*!40000 ALTER TABLE `categorieen` DISABLE KEYS */;
+INSERT INTO `categorieen` VALUES
+    (8,'Fictie',4,1,1,10.00),
+    (12,'Geschiedenis & Politiek',4,1,1,10.00),
+    (13,'Mens & Maatschappij',4,1,1,10.00),
+    (14,'Economie & Management',4,1,1,10.00),
+    (9,'Pop',5,1,1,10.00),
+    (15,'Jazz',5,1,1,10.00),
+    (16,'Rock',5,1,1,10.00),
+    (17,'Opera',5,1,1,10.00),
+    (10,'Strijkijzers',6,1,1,10.00),
+    (18,'Vaatwassers',6,1,1,10.00),
+    (19,'Koelkasten',6,1,1,10.00),
+    (20,'Diepvriezers',6,1,1,10.00),
+    (11,'Herenkleding',7,1,1,10.00),
+    (21,'Jongenskleding',7,1,1,10.00),
+    (22,'Dameskleding',7,1,1,10.00),
+    (23,'Meisjeskleding',7,0,1,10.00);
+/*!40000 ALTER TABLE `categorieen` ENABLE KEYS */;
+UNLOCK TABLES;
