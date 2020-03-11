aebe7334eeef903cb28a0db3f22353f0c0b70055
Mulangu <tryanswer42@gmail.com>
2020-03-11 11:32:52 +0100

    Menu HTML Created Producten toev. Categorien toev.
    +Css MenuElement : center text

---
+++ src/main/java/be/vdab/justgetit/controllers/MenuController.java
@@ -0,0 +1,26 @@
+package be.vdab.justgetit.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class MenuController {
+
+    @Value("1.0.0")
+    private String webVersion;
+
+
+    @GetMapping
+    public ModelAndView index() {
+
+        ModelAndView modelAndView = new ModelAndView("menu");
+
+        return modelAndView;
+
+    }
+
+}
--- src/main/java/be/vdab/justgetit/exceptions/CategorieNietGevondenException.java
+++
@@ -1,7 +0,0 @@
-package be.vdab.justgetit.exceptions;
-
-public class CategorieNietGevondenException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-}
