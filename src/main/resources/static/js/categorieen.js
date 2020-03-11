ce0acc81abcb62dad6edb81b2e37f9af3f12a0c6
luka.cukeric <lukacukeric77@gmail.com>
2020-03-11 11:56:57 +0100

    Made: - Category page - hidden & shown added

---
+++ src/main/resources/static/js/categorieen.js
@@ -0,0 +1,10 @@
+function flipVisibilty () {
+    let element = document.getElementById("parent");
+    if (element.style.display==="block"){
+        element.style.display="none";
+    }else{
+        element.style.display="block";
+    }
+
+}
+
