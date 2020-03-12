029a4609342d9142b3927a073bedf0cf060059d7
Vavorit <junkyard@telenet.be>
2020-03-12 12:14:08 +0100

    opzoeken van categorie voor insert (Lock tables probleem)

--- src/main/resources/fillDBcategorieen.sql
+++ src/main/resources/fillDBcategorieen.sql
@@ -1,25 +1,33 @@
 LOCK TABLES `categorieen` WRITE;
-/*!40000 ALTER TABLE `categorieen` DISABLE KEYS */;
-INSERT INTO `categorieen` (id, naam, parentId, isSubCategorie, winstmargeIsPercentage, winstMarge) VALUES
-    (4,'boeken',NULL,0,1,10.00,1),
-    (5,'CD\'s',NULL,0,1,10.00,1),
-    (6,'elektronische toestellen',NULL,0,1,10.00,1),
-    (7,'kleding',NULL,0,1,10.00,1),
-    (8,'Fictie',4,1,1,10.00),
-    (12,'Geschiedenis & Politiek',4,1,1,10.00),
-    (13,'Mens & Maatschappij',4,1,1,10.00),
-    (14,'Economie & Management',4,1,1,10.00),
-    (9,'Pop',5,1,1,10.00),
-    (15,'Jazz',5,1,1,10.00),
-    (16,'Rock',5,1,1,10.00),
-    (17,'Opera',5,1,1,10.00),
-    (10,'Strijkijzers',6,1,1,10.00),
-    (18,'Vaatwassers',6,1,1,10.00),
-    (19,'Koelkasten',6,1,1,10.00),
-    (20,'Diepvriezers',6,1,1,10.00),
-    (11,'Herenkleding',7,1,1,10.00),
-    (21,'Jongenskleding',7,1,1,10.00),
-    (22,'Dameskleding',7,1,1,10.00),
-    (23,'Meisjeskleding',7,0,1,10.00);
-/*!40000 ALTER TABLE `categorieen` ENABLE KEYS */;
+INSERT INTO `categorieen` (naam, parentId, isSubCategorie, winstmargeIsPercentage, winstMarge) VALUES
+    ('boeken',NULL,0,1,10.00),
+    ('CD\'s',NULL,0,1,10.00),
+    ('elektronische toestellen',NULL,0,1,10.00),
+    ('kleding',NULL,0,1,10.00);
+select id from categorieen where naam='boeken' into @parent;
+INSERT INTO `categorieen` (naam, parentId, isSubCategorie, winstmargeIsPercentage, winstMarge) VALUES
+    ('Fictie',@parent,1,1,10.00),
+    ('Geschiedenis & Politiek',@parent,1,1,10.00),
+    ('Mens & Maatschappij',@parent,1,1,10.00),
+    ('Economie & Management',@parent,1,1,10.00);
+select id from categorieen where naam='CD\'s' into @parent;
+INSERT INTO `categorieen` (naam, parentId, isSubCategorie, winstmargeIsPercentage, winstMarge) VALUES
+    ('Pop',@parent,1,1,10.00),
+    ('Jazz',@parent,1,1,10.00),
+    ('Rock',@parent,1,1,10.00),
+    ('Opera',@parent,1,1,10.00);
+select id from categorieen where naam='elektronische toestellen' into @parent;
+INSERT INTO `categorieen` (naam, parentId, isSubCategorie, winstmargeIsPercentage, winstMarge) VALUES
+    ('Strijkijzers',@parent,1,1,10.00),
+    ('Vaatwassers',@parent,1,1,10.00),
+    ('Koelkasten',@parent,1,1,10.00),
+    ('Diepvriezers',@parent,1,1,10.00);
+select id from categorieen where naam='kleding' into @parent;
+INSERT INTO `categorieen` (naam, parentId, isSubCategorie, winstmargeIsPercentage, winstMarge) VALUES
+    ('Herenkleding',@parent,1,1,10.00),
+    ('Jongenskleding',@parent,1,1,10.00),
+    ('Dameskleding',@parent,1,1,10.00),
+    ('Meisjeskleding',@parent,0,1,10.00);
 UNLOCK TABLES;
+
+select * from categorieen;
--- src/main/resources/fillDBproducten.sql
+++ src/main/resources/fillDBproducten.sql
@@ -1,35 +1,55 @@
-LOCK TABLES `producten` WRITE;
+LOCK TABLES `producten` WRITE, categorieen READ;
 /*!40000 ALTER TABLE `producten` DISABLE KEYS */;
-INSERT INTO `producten` (id,naam,aankoopprijs,verkoopprijs,categorie,voorraad,minimaleBestelhoeveelheid) VALUES
-    (1,'Duin Messias',20,30,8,20,1),
-    (30,'God-Keizer op Duin',20,30,8,20,1),
-    (31,'Duin',20,30,8,20,1),
-    (32,'Kinderen van Duin',20,30,8,20,1),
-    (33,'Duin kapitel',20,30,8,20,1),
-    (34,'Ketters van Duin',20,30,8,20,1),
-    (35,'Hitchhikers guide to the galaxy',20,30,8,20,1),
-    (2,'Frans voor beginners',20,30,13,20,1),
-    (3,'1500-1599',20,30,12,20,1),
-    (4,'1600-1699',20,30,12,20,1),
-    (5,'1700-1799',20,30,12,20,1),
-    (6,'slippery when wet',20,30,16,20,1),
-    (7,'De Notenkraker',20,30,17,20,1),
-    (8,'Bad out of Hell',20,30,9,20,1),
-    (9,'Abba greatest hits I',20,30,9,20,1),
-    (10,'Abba greatest hits II',20,30,9,20,1),
-    (11,'Boxer shorts big size',20,30,11,20,1),
-    (12,'Heren funny socks',20,30,11,20,1),
-    (13,'Overhemd blok 2568',20,30,11,20,1),
-    (14,'Lingerie rode kant C',20,30,22,20,1),
-    (15,'Rok bloem lang',20,30,22,20,1),
-    (16,'Jurk bruids wit',20,30,22,20,1),
-    (17,'Miele superkoel B12',20,30,19,20,1),
-    (18,'Miele superkoel B11',20,30,19,20,1),
-    (19,'Miele superkoel B10',20,30,19,20,1),
-    (20,'Miele Eco vaat 5',20,30,18,20,1),
-    (21,'Miele Eco vaat 6',20,30,18,20,1),
-    (22,'Miele Eco vaat 7',20,30,18,20,1),
-    (23,'Miele Eco vaat 8',20,30,18,20,1),
-    (24,'Miele Eco vaat 9',20,30,18,20,1);
+select id from categorieen where naam='Fictie' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Duin Messias',20,30,@parent,20,1),
+    ('God-Keizer op Duin',20,30,@parent,20,1),
+    ('Duin',20,30,@parent,20,1),
+    ('Kinderen van Duin',20,30,@parent,20,1),
+    ('Duin kapitel',20,30,@parent,20,1),
+    ('Ketters van Duin',20,30,@parent,20,1),
+    ('Hitchhikers guide to the galaxy',20,30,@parent,20,1);
+select id from categorieen where naam='Mens & Maatschappij' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Frans voor beginners',20,30,@parent,20,1);
+select id from categorieen where naam='Geschiedenis & Politiek' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('1500-1599',20,30,@parent,20,1),
+    ('1600-1699',20,30,@parent,20,1),
+    ('1700-1799',20,30,@parent,20,1);
+select id from categorieen where naam='Rock' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Slippery when wet',20,30,@parent,20,1);
+select id from categorieen where naam='Opera' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('De Notenkraker',20,30,@parent,20,1);
+select id from categorieen where naam='Pop' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Bat out of Hell',20,30,@parent,20,1),
+    ('Abba greatest hits I',20,30,@parent,20,1),
+    ('Abba greatest hits II',20,30,@parent,20,1);
+select id from categorieen where naam='Herenkleding' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Boxer shorts big size',20,30,@parent,20,1),
+    ('Heren funny socks',20,30,@parent,20,1),
+    ('Overhemd blok 2568',20,30,@parent,20,1);
+select id from categorieen where naam='Dameskleding' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Lingerie rode kant C',20,30,@parent,20,1),
+    ('Rok bloem lang',20,30,@parent,20,1),
+    ('Jurk bruids wit',20,30,@parent,20,1);
+select id from categorieen where naam='Koelkasten' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Miele superkoel B12',20,30,@parent,20,1),
+    ('Miele superkoel B11',20,30,@parent,20,1),
+    ('Miele superkoel B10',20,30,@parent,20,1);
+select id from categorieen where naam='Vaatwassers' into @parent;
+INSERT INTO `producten` (naam, aankoopprijs, verkoopprijs, categorie, voorraad, minimaleBestelHoeveelheid) VALUES
+    ('Miele Eco vaat 5',20,30,@parent,20,1),
+    ('Miele Eco vaat 6',20,30,@parent,20,1),
+    ('Miele Eco vaat 7',20,30,@parent,20,1),
+    ('Miele Eco vaat 8',20,30,@parent,20,1),
+    ('Miele Eco vaat 9',20,30,@parent,20,1);
+
 /*!40000 ALTER TABLE `producten` ENABLE KEYS */;
 UNLOCK TABLES;
